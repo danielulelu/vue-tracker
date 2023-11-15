a55bdf97ae4d4e3e8b8a50b0c1a8e5a8c539f05e
Eleven <danielulelu@gmail.com>
2023-11-15 12:46:08 +0300

    vue task tracker

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container">
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+  </div>
+</template>
+
+<script>
+  import AddTask from "./components/AddTask.vue";
+  import Header from "./components/Header.vue";
+  import Tasks from "./components/Tasks.vue";
+
+  export default {
+    name: "App",
+    components: {
+      Header,
+      Tasks,
+      AddTask,
+    },
+    data() {
+      return {
+        tasks: [],
+        showAddTask: false
+      };
+    },
+    methods: {
+      toggleAddTask() {
+        this.showAddTask = !this.showAddTask;
+      },
+      addTask(task) {
+        this.tasks = [...this.tasks, task];
+      },
+      deleteTask(id) {
+        if (confirm("Are you sure you want to delete?")) {
+          this.tasks = this.tasks.filter((task) => task.id !== id);
+        }
+      },
+    },
+    created() {
+      this.tasks = [
+        {
+          id: 1,
+          text: "Grocery",
+          day: "February 15th at 3:00 p.m",
+          reminder: true,
+        },
+        {
+          id: 2,
+          text: "School meeting",
+          day: "April 29th at 10:00 a.m",
+          reminder: true,
+        },
+        {
+          id: 3,
+          text: "Family gathering",
+          day: "December 26th at 12:00 p.m",
+          reminder: false,
+        },
+      ];
+    },
+  };
+</script>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: "Poppins", sans-serif;
+  }
+
+  .container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 400px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+  }
+
+  .btn {
+    display: inline-block;
+    background: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+  }
+
+  .btn:focus {
+    outline: none;
+  }
+
+  .btn:active {
+    transform: scale(0.98);
+  }
+
+  .btn-block {
+    display: block;
+    width: 100%;
+  }
+</style>
